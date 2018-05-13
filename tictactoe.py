1ad0120e755b1faa345c704030de27e7937569cd
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-05-13 17:21:57 +0100

    Added game logic, refactored board code, place marker function and win check function to account for indexes 1-9 instead of 0-8

--- tictactoe.py
+++ tictactoe.py
@@ -4,21 +4,18 @@ import random
 
 def display_board(board):
     print(' ')
-    print(board[0] + "|" + board[1] + "|" + board[2])
+    print(board[1] + "|" + board[2] + "|" + board[3])
     print("-----")
-    print(board[3] + "|" + board[4] + "|" + board[5])
+    print(board[4] + "|" + board[5] + "|" + board[6])
     print("-----")
-    print(board[6] + "|" + board[7] + "|" + board[8])
+    print(board[7] + "|" + board[8] + "|" + board[9])
     print(' ')
 
 def player_input():
     player1 = ''
+    while (player1 != 'X') and (player1 != 'O'):
+        player1 = input("Please choose 'X' or 'O': ")
 
-    # To ensure the player only chooses a valid marker.
-    while (player1 != "X") and (player1 != "O"):
-        player1 = input("Please choose 'X' or 'O': " )
-
-    # Setting player two's marker.
     if (player1 == "X"):
         player2 = "O"
     else:
@@ -27,25 +24,24 @@ def player_input():
     return (player1, player2)
 
 def place_marker(board, marker, position):
-    position -= 1
     board[position] = marker
 
 def win_check(board, mark):
-    if (mark == board[0] and mark == board[1] and mark == board[2]):
+    if (mark == board[1] and mark == board[2] and mark == board[3]):
         return True
-    elif (mark == board[3] and mark == board[4] and mark == board[5]):
+    elif (mark == board[4] and mark == board[5] and mark == board[6]):
         return True
-    elif (mark == board[6] and mark == board[7] and mark == board[8]):
-        return True
-    elif (mark == board[0] and mark == board[3] and mark == board[6]):
+    elif (mark == board[7] and mark == board[8] and mark == board[9]):
         return True
     elif (mark == board[1] and mark == board[4] and mark == board[7]):
         return True
     elif (mark == board[2] and mark == board[5] and mark == board[8]):
         return True
-    elif (mark == board[0] and mark == board[4] and mark == board[8]):
+    elif (mark == board[3] and mark == board[6] and mark == board[9]):
         return True
-    elif (mark == board[2] and mark == board[4] and mark == board[6]):
+    elif (mark == board[1] and mark == board[5] and mark == board[9]):
+        return True
+    elif (mark == board[3] and mark == board[5] and mark == board[7]):
         return True
     else:
         return False
@@ -60,8 +56,8 @@ def choose_first():
 def space_check(board, position):
     return (board[position] == " ")
 
-def full_board_check:
-    for index in range(0, 9):
+def full_board_check(board):
+    for index in range(0, 8):
         if space_check(board, index):
             return False
     return True
@@ -69,11 +65,62 @@ def full_board_check:
 def player_choice(board):
     accepted_choice = False
     while accepted_choice == False:
-        position = int(input("Please enter your next position"))
-        if (space_check(board, position))
+        position = int(input("Please enter your next position: "))
+        if (space_check(board, position)):
             accepted_choice = True
             return position
 
 def replay():
-    choice = input("Do you want to play again? Y/N: ")
-    return (choice == "Y")
+    choice = input("Do you want to play again? y/n: ")
+    return (choice == "y")
+
+# All required functions listed above, game logic to be played follows below.
+while True:
+
+    board = [ ' ' ] * 10
+    player1, player2 = player_input()
+    player_to_start = choose_first()
+    print(player_to_start + " will start.")
+
+    game_won = False
+    while game_won == False:
+
+        # Game logic for player1 to start
+        if player1 == player_to_start:
+            display_board(board)
+            position = player_choice(board)
+            place_marker(board, player1, position)
+
+            if win_check(board, player1):
+                display_board(board)
+                print("Congrats player 1!!!")
+                game_won = True
+
+            if full_board_check(board):
+                display_board(board)
+                print("It's a draw!")
+                game_won = True
+
+            player_to_start = player2
+
+        # Game logic for player two to start, exact same as player one.
+        # Simply repeats the same methods until there is a winner or a draw.
+        if player2 == player_to_start:
+            display_board(board)
+            position = player_choice(board)
+            place_marker(board, player2, position)
+
+            if win_check(board, player2):
+                display_board(board)
+                print("Congrats player 2!!!")
+                game_won = True
+
+            if full_board_check(board):
+                display_board(board)
+                print("It's a draw!")
+                game_won = True
+
+            player_to_start = player1
+
+    if replay() != True:
+        break
